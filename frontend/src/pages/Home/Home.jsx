f6aeb0e02dcdc6d25df72ca0e3a388a8c124afc1
Ctrl-E <73417469+Ctrl-E@users.noreply.github.com>
2024-06-02 18:32:10 +0200

    Homepage gallery section coded

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { Gallery } from "./Gallery/Gallery";
 import { HeroContainer } from "./Hero/HeroContainer";
 
 export const Home = () => {
   return (
     <div>
       <HeroContainer />
+      <div className="max-w-screen-xl mx-auto">
+        <Gallery />
+      </div>
     </div>
   );
 };
